9b142e19599135785a1b9fdbb70a8cd57f38583d
Dmitry Shaplyko <dzmitry_shaplyka@epam.com>
2018-02-13 20:59:48 +0300

    form js added

--- e2e/test/pageObjects/webelements/forms/form.js
+++ e2e/test/pageObjects/webelements/forms/form.js
@@ -7,14 +7,26 @@ class Form extends WebElement {
 
     getElement(elm) {
         switch (elm.toLowerCase()) {
-            case 'first name field': return new Field (browser.element('#form-firstName')); break;
-            case 'last name field': return new Field (browser.element('#form-lastName')); break; 
-            case 'email field': return new Field (browser.element("input[name='email']")); break;
-            case 'user name field': return new Field (browser.element('#form-username')); break;
-            case 'password field': return new Field (browser.element("input[name='password']")); break;
-            case 'birthday field': return new Field (browser.element("#date-picker-birthday")); break;
-            case 'login button': return new Button (browser.element("button[name='login']")); break;
-            case 'register button': return new Button (browser.element(".login-footer a")); break;
+            case 'first name field': 
+                return new Field (browser.element('#form-firstName'));
+            case 'last name field': 
+                return new Field (browser.element('#form-lastName'));
+            case 'email field': 
+                return new Field (browser.element("input[name='email']"));
+            case 'user name field': 
+                return new Field (browser.element('#form-username'));
+            case 'password field': 
+                return new Field (browser.element("input[name='password']"));
+            case 'birthday field': 
+                return new Field (browser.element("#date-picker-birthday"));
+            case 'login button': 
+                return new Button (browser.element("button[name='login']"));
+            case 'register button': 
+                return new Button (browser.element("button[name='register']"));
+            case 'sign up button':
+                return new Button (browser.element(".login-footer a"));
+            case 'sign in button':
+                return new Button (browser.element(".logout-footer a"));
         }
     };
 }
