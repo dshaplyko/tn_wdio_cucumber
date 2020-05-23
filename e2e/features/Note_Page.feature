b2b2275edec5eb051beafb762c091be6cb0338b2
Dmitry Shaplyko <dmitryshaplyko@MacBook-Pro-Dmitry.local>
2020-05-23 12:06:40 +0300

    small changes

--- e2e/features/Note_Page.feature
+++ e2e/features/Note_Page.feature
@@ -1,4 +1,4 @@
-@Wip
+@Run
 Feature: Note Feature
 	As a user I want to be able to navigate and use the Note's page
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -54,7 +54,7 @@ exports.config = {
 	baseUrl: 'https://t-notes.herokuapp.com/',
 	//
 	// Default timeout for all waitFor* commands.
-	waitforTimeout: 10000,
+	waitforTimeout: 30000,
 	//
 	// Default timeout in milliseconds for request
 	// if browser driver or grid doesn't send response
